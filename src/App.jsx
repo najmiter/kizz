e4d7d61438da69126eb9e94f9fe084e86f5efbba
najmiter <realnajmiter@gmail.com>
2024-03-19 09:05:50 +0500

    some new stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
 import "./App.css";
 
 function App() {
-    return <h1>hello</h1>;
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main className="main">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+                quis.
+            </p>
+        </main>
+    );
+}
